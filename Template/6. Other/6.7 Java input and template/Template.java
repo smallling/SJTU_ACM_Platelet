59c09632d6d4a377642672f6683c5b5080ebd021
smallling <lhy526620793@qq.com>
2018-09-15 19:23:18 +0800

    Update Template.java

--- Template/6. Other/6.7 Java input and template/Template.java
+++ Template/6. Other/6.7 Java input and template/Template.java
@@ -1,9 +1,21 @@
-import java.io.*;
-import java.math.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Template {
-    // Fast Input
+    // Input
     private static BufferedReader reader;
     private static StringTokenizer tokenizer;
 
@@ -25,15 +37,18 @@ public class Template {
         return Double.parseDouble(next());
     }
 
-    private static BigInteger bigInteger() {
+    private static BigInteger nextBigInteger() {
         return new BigInteger(next());
     }
 
     public static void main(String[] args) {
         reader = new BufferedReader(new InputStreamReader(System.in));
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext())
+            scanner.next();
     }
 
-    // BigDecimal
+    // BigInteger & BigDecimal
     private static void bigDecimal() {
         BigDecimal a = BigDecimal.valueOf(1.0);
         BigDecimal b = a.setScale(50, RoundingMode.HALF_EVEN);
@@ -41,7 +56,10 @@ public class Template {
         // if scale omitted, b.scale is used
         BigDecimal d = c.divide(b, 50, RoundingMode.HALF_EVEN);
         // since Java 9
-        BigDecimal e = d.sqrt(new MathContext(50, RoundingMode.HALF_EVEN));
+        // BigDecimal e = d.sqrt(new MathContext(50, RoundingMode.HALF_EVEN));
+        BigDecimal x = new BigDecimal(BigInteger.ZERO);
+        BigInteger y = BigDecimal.ZERO.toBigInteger(); // RoundingMode.DOWN
+        y = BigDecimal.ZERO.setScale(0, RoundingMode.HALF_EVEN).unscaledValue();
     }
 
     // sqrt for Java 8
@@ -53,6 +71,21 @@ public class Template {
         BigDecimal TWO = BigDecimal.valueOf(2L);
         for (int i = 1; i <= scale; i++)
             ans = ans.add(a.divide(ans, scale, mode)).divide(TWO, scale, mode);
+        return ans;
+    }
+
+    private static BigInteger sqrt(BigInteger a) {
+        BigInteger about = BigInteger.ZERO.setBit(a.bitLength() / 2);
+        return sqrt(new BigDecimal(a.toString()), new BigDecimal(about.toString())).setScale(0, RoundingMode.FLOOR).unscaledValue();
+    }
+
+    private static BigDecimal sqrt(BigDecimal a, BigDecimal initial) {
+        if (a.equals(BigDecimal.ZERO))
+            return BigDecimal.ZERO;
+        a = a.setScale(50, RoundingMode.HALF_EVEN);
+        BigDecimal ans = initial;
+        for (int i = 1; i <= 10; i++)
+            ans = ans.add(a.divide(ans, RoundingMode.HALF_EVEN)).divide(BigDecimal.valueOf(2), RoundingMode.HALF_EVEN);
         return ans;
     }
 
@@ -89,6 +122,7 @@ public class Template {
             if (a > b) return -1;
             return 1;
         });
+        Arrays.sort(new Integer[10], Comparator.comparingInt((a) -> (int) a).reversed());
         long a = 1_000_000_000_000_000_000L;
         int b = Integer.MAX_VALUE;
         int c = 'a';
