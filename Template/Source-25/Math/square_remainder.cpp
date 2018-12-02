fd14db6ab5975df5bae255aa0fb62b56d74b826d
wu-qing-157 <wuqing157@gmail.com>
2018-12-03 00:05:25 +0800

    [Update] simply some code

--- Template/Source-25/Math/square_remainder.cpp
+++ Template/Source-25/Math/square_remainder.cpp
@@ -1,8 +1,7 @@
 void multiply(ll &c,ll &d,ll a,ll b,ll w){
   int cc = (a*c+b*d%MOD*w)%MOD;
   int dd = (a*d+b*c)%MOD;
-  c = cc,d = dd;
-}
+  c = cc,d = dd;}
 bool solve(int n,int &x){
   if(n==0) return x = 0,true;
   if(MOD==2) return x = 1,true;
@@ -16,8 +15,6 @@ bool solve(int n,int &x){
   }while(power(w,MOD/2,MOD)!=MOD-1);
   for(int times = (MOD+1)/2;times;times >>= 1){
     if(times&1) multiply(c,d,a,b,w);
-    multiply(a,b,a,b,w);
-  }
+    multiply(a,b,a,b,w); }
   // x = (a + sqrt(w)) ^ ((p + 1) / 2)
-  return x = c,true;
-}
+  return x = c,true; }
