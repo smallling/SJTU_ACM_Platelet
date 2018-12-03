1d0561ba214afd29d880a839837e4c89f90c2a98
smallling <lhy526620793@qq.com>
2018-12-03 10:34:23 +0800

    [Update]

--- Template/Source-25/GraphTheory/sap.cpp
+++ Template/Source-25/GraphTheory/sap.cpp
@@ -1,6 +1,5 @@
 void SAP(int n,int st,int ed){
-  for(int i=1;i<=n;i++)now[i]=son[i];
-  sumd[0]=n;
+  for(int i=1;i<=n;i++)now[i]=son[i];sumd[0]=n;
   int flow=inf,x=st;
   while(dis[st]<n){
     back[x]=flow;
@@ -15,24 +14,16 @@ void SAP(int n,int st,int ed){
           while(x!=st){
             edge[pre[x]].f-=flow;
             edge[pre[x]^1].f+=flow;
-            x=edge[pre[x]].x;
-          }
-          flow=inf;
-        }
-        break;
-      }
-    }
+            x=edge[pre[x]].x;}
+          flow=inf;}
+        break;}}
     if(flag)continue;
     int minn=n-1,tmp;
     for(int i=son[x];i!=-1;i=edge[i].next){
-      int y = edge[i].y;
+      int y=edge[i].y;
       if(edge[i].f&&dis[y]<minn){
-        minn = dis[y];tmp=i;
-      }
-    }
+        minn=dis[y];tmp=i;}}
     now[x]=tmp;
     if(!(--sumd[dis[x]]))return;
     sumd[dis[x]=minn+1]++;
-    if(x!=st)flow=back[x=edge[pre[x]].x];
-  }
-}
+    if(x!=st)flow=back[x=edge[pre[x]].x];}}
