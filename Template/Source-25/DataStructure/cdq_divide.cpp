c0f4736c36b617e2c4948ab3ff6e1239330c7e69
chentong1023 <chentong1023@qq.com>
2018-12-06 00:05:10 +0800

    20181206_ct_source-25

--- Template/Source-25/DataStructure/cdq_divide.cpp
+++ Template/Source-25/DataStructure/cdq_divide.cpp
@@ -1,28 +1,15 @@
-struct event {
-  int x,y,id,opt,ans;
-} t[maxn],q[maxn];
-void cdq(int left,int right){
-  if(left==right) return;
-  int mid = left+right>>1;
-  cdq(left,mid);
-  cdq(mid+1,right);
-  //分成若干个子问题
-  ++now;
-  for(int i = left,j = mid+1;j<=right;++j){
-    for(;i<=mid&&q[i].x<=q[j].x;++i)
-      if(!q[i].opt)
-        add(q[i].y,q[i].ans);
+struct event{int x,y,id,opt,ans;}t[maxn],q[maxn];
+void cdq(int l,int r){
+  if(l==r)return; int m=l+r>>1;
+  cdq(l,m); cdq(m+1,r);
+  for(int i=l,j=m+1;j<=r;++j){
+    for(;i<=m&&q[i].x<=q[j].x;++i)
+      if(!q[i].opt) add(q[i].y,q[i].ans);
     //考虑前面的修改操作对后面的询问的影响
-    if(q[j].opt)
-      q[j].ans += query(q[j].y);
-  }
-  int i,j,k = 0;
-  //以下相当于归并排序
-  for(i = left,j = mid+1;i<=mid&&j<=right;){
-    if(q[i].x<=q[j].x) t[k++] = q[i++];
-    else t[k++] = q[j++];
-  }
-  for(;i<=mid;)t[k++] = q[i++];
-  for(;j<=right;)t[k++] = q[j++];
-  for(int i = 0;i<k;++i)q[left+i] = t[i];
-}
+    if(q[j].opt) q[j].ans += query(q[j].y);}
+  int i,j,k=0; //以下相当于归并排序
+  for(i=l,j=m+1;i<=m&&j<=r;){
+    if(q[i].x<=q[j].x)t[k++]=q[i++];
+    else t[k++]=q[j++];}
+  while(i<=m)t[k++]=q[i++];while(j<=r)t[k++]=q[j++];
+  for(int i=0;i<k;++i)q[l+i]=t[i];}
