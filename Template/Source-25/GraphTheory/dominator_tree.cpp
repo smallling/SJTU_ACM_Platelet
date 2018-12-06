4fabf9cae279b13c239f76edd502a72f5bb1ce81
wu-qing-157 <wuqing157@gmail.com>
2018-12-06 15:22:59 +0800

    [Update] tex-25

--- Template/Source-25/GraphTheory/dominator_tree.cpp
+++ Template/Source-25/GraphTheory/dominator_tree.cpp
@@ -1,25 +1,19 @@
 struct Dominator_Tree {
   int n,s,cnt;
-  int dfn[N],id[N],pa[N],semi[N],idom[N],p[N],
-    mn[N];
+  int dfn[N],id[N],pa[N],semi[N],idom[N],p[N],mn[N];
   vector<int> e[N],dom[N],be[N];
   void ins(int x,int y){e[x].push_back(y);}
   void dfs(int x){
-    dfn[x] = ++cnt;
-    id[cnt] = x;
+    dfn[x] = ++cnt; id[cnt] = x;
     for(auto i:e[x]){
       if(!dfn[i])dfs(i),pa[dfn[i]] = dfn[x];
-      be[dfn[i]].push_back(dfn[x]);
-    }
-  }
+      be[dfn[i]].push_back(dfn[x]); }}
   int get(int x){
     if(p[x]!=p[p[x]]){
       if(semi[mn[x]]>semi[get(p[x])])
         mn[x] = get(p[x]);
-      p[x] = p[p[x]];
-    }
-    return mn[x];
-  }
+      p[x] = p[p[x]]; }
+    return mn[x]; }
   void LT(){
     for(int i = cnt;i>1;i--){
       for(auto j:be[i])
@@ -28,17 +22,12 @@ struct Dominator_Tree {
       int x = p[i] = pa[i];
       for(auto j:dom[x])
         idom[j] = (semi[get(j)]<x ? get(j) : x);
-      dom[x].clear();
-    }
+      dom[x].clear(); }
     for(int i = 2;i<=cnt;i++){
       if(idom[i]!=semi[i])idom[i] = idom[idom[i]];
-      dom[id[idom[i]]].push_back(id[i]);
-    }
-  }
+      dom[id[idom[i]]].push_back(id[i]); }}
   void build(){
     for(int i = 1;i<=n;i++)
       dfn[i] = 0,dom[i].clear(),be[i].clear(),
         p[i] = mn[i] = semi[i] = i;
-    cnt = 0,dfs(s),LT();
-  }
-};
+    cnt = 0,dfs(s),LT(); }};
--- Template/Source-25/Others/.vimrc
+++ Template/Source-25/Others/.vimrc
@@ -4,8 +4,8 @@ ino <tab> <c-n>
 ino <s-tab> <tab>
 au bufwinenter * winc L
 nm <f6> ggVG"+y
-nm <f7> :w<cr>:!rm ##<cr>:make<cr>
-nm <f8> :!@@<cr>
-nm <f9> :!@@ < in<cr>
-nm <s-f9> :!(time @@ < in &> out) &>> out<cr>:sp out<cr>
-au filetype cpp cm @@ ./a.out | cm ## a.out | se cin fdm=syntax mp=g++\ %\ -std=c++11\ -Wall\ -Wextra\ -Wconversion\ -O2
+nm <f7> :w<cr>:make %<<cr>
+nm <f8> :!./%<<cr>
+nm <f9> :!./%< < in<cr>
+nm <s-f9> :!(time ./%< < in &> out) &>> out<cr>:sp out<cr>
+au filetype cpp se cin fdm=syntax
